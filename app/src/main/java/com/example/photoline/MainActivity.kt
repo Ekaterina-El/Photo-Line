7f776ee0a7fc550a20fc789a2135b2c4ffbcfaad
Ekaterina-El <hackerrussia@yandex.ru>
2021-09-21 19:44:52 +0500

    Styling

--- app/src/main/java/com/example/photoline/MainActivity.kt
+++ app/src/main/java/com/example/photoline/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.example.photoline
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.photoline.ui.feed.FeedActivity
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("UseSupportActionBar")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         startActivity(Intent(this, FeedActivity::class.java))
     }
